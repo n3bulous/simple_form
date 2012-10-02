8f07d0ea1abdad7c36a4039a686b56ffca01e3f5
Kevin McFadden <kmcfadden@gmail.com>
2012-10-01 22:41:49 -0600

    leaving html5_type in classes

--- lib/simple_form/inputs/html5_string_input.rb
+++ lib/simple_form/inputs/html5_string_input.rb
@@ -4,7 +4,7 @@ module SimpleForm
       HTML5_TYPES = [:html5_date, :html5_datetime, :html5_time]
 
       def input
-        input_html_classes.reject! {|klass| HTML5_TYPES.include?(klass) }
+        #input_html_classes.reject! {|klass| HTML5_TYPES.include?(klass) }
         input_html_options[:type] ||= input_type.to_s.sub(/^html5_/, '') if html5?
         @builder.text_field(attribute_name, input_html_options)
       end
